b2f89acf504b8bfe2ff0e595f1fe2c16b52f1237
Melanie Burger <melanie.brgr@gmail.com>
2024-03-01 22:53:59 +0100

    feat(ew): :sparkles:

--- 00-everyweeks/week08/src/main.rs
+++ 00-everyweeks/week08/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::io::{self};
 
 fn main() {
-    let pinyin_hanzi_map = HashMap::from([
+    let pinyin_to_hanzi = HashMap::from([
         ("wǒ".to_string(), "我".to_string()),
         ("nǐ".to_string(), "你".to_string()),
         ("hǎo".to_string(), "好".to_string()),
@@ -10,16 +10,21 @@ fn main() {
         ("jiàn".to_string(), "见".to_string()),
     ]);
 
-    let hanzi = pinyin_hanzi_map.values().nth(0).unwrap();
-    println!("What is the pinyin for {}?", hanzi);
+    let random_hanzi = pinyin_to_hanzi.values().nth(0).unwrap();
+
+    println!("What is the pinyin for {}?", &random_hanzi);
 
     let mut input = String::new();
+
     io::stdin().read_line(&mut input).unwrap();
 
-    let fallback = "?".to_string();
-    let pinyin = pinyin_hanzi_map
-        .get(input.trim())
-        .unwrap_or_else(|| &fallback);
+    let maybe_hanzi = pinyin_to_hanzi.get(input.trim());
 
-    println!("pinyin found! ({}) ", pinyin);
+    let you_are_right = "你不错了！👍".to_string();
+    let you_are_wrong = "你错了！👎".to_string();
+
+    match maybe_hanzi {
+        Some(_) => println!("{}", you_are_right),
+        None => println!("{}", you_are_wrong),
+    }
 }
