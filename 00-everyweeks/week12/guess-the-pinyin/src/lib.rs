4b7d8c1b0802d31849f8ad08919e3fdecc993133
Melanie Burger <melanie.brgr@gmail.com>
2024-03-24 17:21:41 +0100

    refactor(ew): :recycle:

--- 00-everyweeks/week12/guess-the-pinyin/src/lib.rs
+++ 00-everyweeks/week12/guess-the-pinyin/src/lib.rs
@@ -1,3 +1,5 @@
+mod hanzi;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -8,37 +10,6 @@ extern "C" {
     pub fn alert(s: &str);
 
     pub fn prompt(s: &str) -> String;
-}
-
-mod hanzi {
-    use rand::Rng;
-    use std::collections::HashMap;
-
-    fn pinyin_to_hanzi() -> HashMap<String, String> {
-        HashMap::from([
-            ("wǒ".to_string(), "我".to_string()),
-            ("nǐ".to_string(), "你".to_string()),
-            ("hǎo".to_string(), "好".to_string()),
-            ("zài".to_string(), "再".to_string()),
-            ("jiàn".to_string(), "见".to_string()),
-            ("jiào".to_string(), "叫".to_string()),
-            ("lǐ".to_string(), "李".to_string()),
-        ])
-    }
-
-    pub fn rand() -> String {
-        let pinyin_to_hanzi = pinyin_to_hanzi();
-        let random_number = rand::thread_rng().gen_range(0..pinyin_to_hanzi.len());
-        return pinyin_to_hanzi
-            .values()
-            .nth(random_number)
-            .unwrap()
-            .to_string();
-    }
-
-    pub fn get_pinyin(hanzi: &str) -> Option<String> {
-        pinyin_to_hanzi().get(hanzi).cloned()
-    }
 }
 
 struct State {
@@ -57,6 +28,7 @@ impl State {
     }
 }
 
+#[allow(dead_code)]
 enum Tone {
     First = 1,  // 1st tone (¯): option + a
     Second = 2, // 2nd tone (´): option + e
