65a4fc51024f0d3edadc925b1111f527b94f8ceb
Melanie Burger <melanie.brgr@gmail.com>
2024-04-15 23:30:52 +0200

    feat(ew): :sparkles:

--- 00-everyweeks/week16/exploring-service-workers/src/app/WebWorker.tsx
+++ 00-everyweeks/week16/exploring-service-workers/src/app/WebWorker.tsx
@@ -2,13 +2,11 @@
 
 export function WebWorker() {
   const runWorker = () => {
-    console.log("Running Web Worker");
-    
-    const worker = new Worker("./worker.ts");
-    worker.addEventListener("message", (e) => {
+    const worker = new Worker("./worker.js");
+    worker.addEventListener("message", (e: MessageEvent) => {
       console.log(e.data);
     });
-  };
+  }
 
   return <button onClick={runWorker} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Run Web Worker</button>;
 }
