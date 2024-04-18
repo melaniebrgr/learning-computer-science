168bbdb4d263f8fa88cafc7c5a93228aefd04bd0
Melanie Burger <melanie.brgr@gmail.com>
2024-04-18 09:30:57 +0200

    feat(ew): :sparkles:

--- 00-everyweeks/week16/exploring-service-workers/src/app/WebWorker.tsx
+++ 00-everyweeks/week16/exploring-service-workers/src/app/WebWorker.tsx
@@ -3,17 +3,46 @@
 import { useEffect, useRef } from "react";
 
 export function WebWorker() {
-  const { current: worker } = useRef(new Worker("./worker.js"));
+  const worker  = useRef<Worker>();
 
   useEffect(() => {
-    worker.addEventListener("message", (e: MessageEvent) => {
-      console.log('Message received in react:', e.data);
+    worker.current = new Worker("./worker.js");
+    worker.current.addEventListener("message", (e: MessageEvent) => {
+      console.log('Is profane:', e.data);
     });
+
+    return () => {
+      worker.current!.terminate();
+    };
   }, []);
 
   const postMessage = () => {
-    worker.postMessage("from react");
+    worker.current!.postMessage("Hello World!");
   }
 
-  return <button onClick={postMessage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Run Web Worker</button>;
+  const postProfanity = () => {
+    worker.current!.postMessage("Hello Mother Fuckers!");
+  }
+
+  return (<>
+    <button onClick={postMessage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Hello World!</button>
+    <button onClick={postProfanity} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Hello Mother Fuckers!</button>
+  </>);
 }
+
+/*
+self.onmessage = (e) => {
+  checkProfanity(e.data);
+}
+
+const checkProfanity = (text) => {
+  const url = new URL('https://www.purgomalum.com/service/containsprofanity');
+  url.searchParams.append('text', text);
+
+  fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      self.postMessage(data);
+    });
+};
+ */
