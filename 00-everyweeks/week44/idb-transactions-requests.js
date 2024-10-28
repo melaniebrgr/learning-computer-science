5d78e0a33d8f24a86ad8deff9b37dfdac006421d
Melanie Burger <melanie.brgr@gmail.com>
2024-10-28 07:18:01 +0100

    feat(ew): :sparkles:

---
+++ 00-everyweeks/week44/idb-transactions-requests.js
@@ -0,0 +1,94 @@
+import { v4 as uuid } from 'https://jspm.dev/uuid';
+
+const DB_ID = 'NotesDB';
+const DB_STORE_ID = 'notesStore';
+const version = 1;
+
+let db;
+let objectStore;
+
+(function initApp() {
+  initDB();
+  document.getElementById('form').addEventListener('submit', handleSubmit);
+})();
+
+function initDB() {
+  const NotesIDBOpenRequest = indexedDB.open(DB_ID, version);
+
+  NotesIDBOpenRequest.addEventListener('error', (error) => {
+    console.error(error);
+  });
+
+  NotesIDBOpenRequest.addEventListener('success', (event) => {
+    db = event.target.result;
+    renderFileList();
+  });
+
+  NotesIDBOpenRequest.addEventListener('upgradeneeded', (event) => {
+    db = event.target.result;
+    console.info(`>> upgradeneeded: DB updated from ${event.oldVersion} to ${event.newVersion}`, db)
+
+    if (!db.objectStoreNames.contains(DB_STORE_ID)) {
+      objectStore = db.createObjectStore(DB_STORE_ID, {
+        keyPath: 'id',
+      })
+    }
+  });
+
+  NotesIDBOpenRequest.addEventListener('blocked', noop);
+};
+
+function handleSubmit (event) {
+  event.preventDefault();
+  const formData = Object.fromEntries((new FormData(this)).entries());
+
+  const tx = createTxForStore(DB_STORE_ID, 'readwrite', () => {
+    clearForm();
+  });
+
+  const request = tx.add({
+    id: uuid(),
+    ...formData,
+  });
+
+  request.onsuccess = noop;
+
+  request.onerror = noop;
+}
+
+function noop () {};
+
+function createTxForStore(storeId, mode, successCallback) {
+  const tx = db.transaction(storeId, mode);
+
+  tx.onerror = noop
+
+  if (successCallback) tx.oncomplete = successCallback;
+
+  return tx.objectStore(DB_STORE_ID);
+}
+
+function clearForm(event) {
+  if (event) event.preventDefault();
+  document.getElementById('form').reset();
+}
+
+function renderFileList() {
+  const fileList = document.getElementById('file-list');
+  fileList.innerHTML = '<li>Loading...</li>';
+
+  const tx = createTxForStore(DB_STORE_ID, 'readonly');
+
+  const request = tx.getAll();
+
+  request.onsuccess = (event) => {
+    fileList.innerHTML = '';
+    event.target.result.forEach((file) => {
+      const li = document.createElement('li');
+      li.textContent = file.title;
+      fileList.appendChild(li);
+    });
+  };
+
+  request.onerror = noop;
+}
