b3582f3ef791b30106e78a970dced8f20b51a210
Melanie Burger <melanie.brgr@gmail.com>
2026-04-04 09:38:45 +0200

    feat(sys): :sparkles:

--- 05-system-design/02-frontend/02-routing/tanstack-router/vite.config.ts
+++ 05-system-design/02-frontend/02-routing/tanstack-router/vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from "vite-plus";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
-export default defineConfig({
-  fmt: {},
-  lint: { options: { typeAware: true, typeCheck: true } },
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-});
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+const config = defineConfig({
+  plugins: [
+    devtools(),
+    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tailwindcss(),
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    viteReact(),
+  ],
+})
+
+export default config
